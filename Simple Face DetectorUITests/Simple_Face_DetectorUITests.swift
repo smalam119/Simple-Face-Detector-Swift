d3732596154b5be1dd8e1f0b9f835596651527a8
Sayed Mahmudul Alam <ma@inov.io>
2016-12-31 12:44:24 +0600

    Initial Commit

---
+++ Simple Face DetectorUITests/Simple_Face_DetectorUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_Face_DetectorUITests.swift
+//  Simple Face DetectorUITests
+//
+//  Created by Sayed Mahmudul Alam on 12/31/16.
+//  Copyright © 2016 Sayed Mahmudul Alam. All rights reserved.
+//
+
+import XCTest
+
+class Simple_Face_DetectorUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
